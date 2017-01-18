711e015e584734bdac4b7f95ebf097222e5d38e7
Johannes Donath <johannesd@torchmind.com>
2017-01-18 17:35:52 +0100

    Implemented a modular message source which may be extended at runtime with further sources.

---
+++ module/src/main/java/tv/dotstart/pandemonium/module/ModuleMessageSource.java
@@ -0,0 +1,92 @@
+/*
+ * Copyright 2017 Johannes Donath <me@dotstart.tv>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.pandemonium.module;
+
+import org.springframework.context.MessageSource;
+import org.springframework.context.NoSuchMessageException;
+import org.springframework.context.support.AbstractMessageSource;
+
+import java.text.MessageFormat;
+import java.util.LinkedList;
+import java.util.Locale;
+import java.util.concurrent.locks.ReadWriteLock;
+import java.util.concurrent.locks.ReentrantReadWriteLock;
+import java.util.stream.Stream;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+/**
+ * Provides a modular message source which resolves against multiple child sources.
+ *
+ * @author <a href="mailto:me@dotstart.tv">Johannes Donath</a>
+ */
+public class ModuleMessageSource extends AbstractMessageSource {
+    private final LinkedList<MessageSource> childSources = new LinkedList<>();
+    private final ReadWriteLock childSourceLock = new ReentrantReadWriteLock();
+
+    /**
+     * Adds a message source to the list of children.
+     */
+    public void addSource(@Nonnull MessageSource source) {
+        this.childSourceLock.writeLock().lock();
+
+        try {
+            this.childSources.add(source);
+        } finally {
+            this.childSourceLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * Removes a message source from the list of children.
+     */
+    public void removeSource(@Nonnull MessageSource source) {
+        this.childSourceLock.writeLock().lock();
+
+        try {
+            this.childSources.remove(source);
+        } finally {
+            this.childSourceLock.writeLock().unlock();
+        }
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nullable
+    @Override
+    protected MessageFormat resolveCode(String code, Locale locale) {
+        this.childSourceLock.readLock().lock();
+
+        try {
+            return this.childSources.stream()
+                    .flatMap((s) -> {
+                        try {
+                            return Stream.of(s.getMessage(code, new Object[0], locale));
+                        } catch (NoSuchMessageException ex) {
+                            return Stream.empty();
+                        }
+                    })
+                    .map((m) -> this.createMessageFormat(m, locale))
+                    .findAny()
+                    .orElseGet(() -> this.createMessageFormat(code, locale));
+        } finally {
+            this.childSourceLock.readLock().unlock();
+        }
+    }
+}

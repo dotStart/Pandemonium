3d0c124bf76b7e3c7566c2a91c348a8b652696e0
Johannes Donath <johannesd@torchmind.com>
2017-01-21 23:19:36 +0100

    Added a set of process related exceptions.

---
+++ memory/src/main/java/tv/dotstart/pandemonium/memory/exception/process/ProcessException.java
@@ -0,0 +1,41 @@
+/*
+ * Copyright 2017 Johannes Donath <me@dotstart.tv>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.pandemonium.memory.exception.process;
+
+/**
+ * Provides a base exception for cases where accessing a process fails.
+ *
+ * @author <a href="mailto:me@dotstart.tv">Johannes Donath</a>
+ */
+public abstract class ProcessException extends RuntimeException {
+
+    public ProcessException() {
+        super();
+    }
+
+    public ProcessException(String s) {
+        super(s);
+    }
+
+    public ProcessException(String s, Throwable throwable) {
+        super(s, throwable);
+    }
+
+    public ProcessException(Throwable throwable) {
+        super(throwable);
+    }
+}

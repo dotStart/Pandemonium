093b3f674baad8f44bd562ca84dd03f0c6abae67
Johannes Donath <johannesd@torchmind.com>
2017-01-26 08:28:00 +0100

    Fixed reference to process interface as well as dependency information.

--- effect/src/main/java/tv/dotstart/pandemonium/effect/EffectFactory.java
+++ effect/src/main/java/tv/dotstart/pandemonium/effect/EffectFactory.java
@@ -21,6 +21,8 @@ import org.controlsfx.tools.Platform;
 import javax.annotation.Nonnegative;
 import javax.annotation.Nonnull;
 
+import tv.dotstart.pandemonium.process.Process;
+
 /**
  * Provides a factory for an effect which may be applied to the program instance or memory in order
  * to change the game behavior persistently or temporarily.

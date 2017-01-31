bda4692983b6578f07bfd62307fdd7449dc81e91
Johannes Donath <johannesd@torchmind.com>
2017-01-31 16:32:25 +0100

    Added tasks to fetch information on endorsements and check for application updates.

---
+++ ui/src/main/java/tv/dotstart/pandemonium/ui/window/splash/EndorsementUpdateTask.java
@@ -0,0 +1,49 @@
+/*
+ * Copyright 2017 Johannes Donath <me@dotstart.tv>
+ * and other copyright owners as documented in the project's IP log.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package tv.dotstart.pandemonium.ui.window.splash;
+
+import javax.annotation.Nonnull;
+
+import tv.dotstart.pandemonium.ui.service.UpdateService;
+
+/**
+ * @author <a href="mailto:me@dotstart.tv">Johannes Donath</a>
+ */
+public class EndorsementUpdateTask implements InitializationTask {
+    private final UpdateService updateService;
+
+    public EndorsementUpdateTask(@Nonnull UpdateService updateService) {
+        this.updateService = updateService;
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Nonnull
+    @Override
+    public String getLocalizationKey() {
+        return "update.endorsement";
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    @Override
+    public void run() throws Exception {
+        this.updateService.fetchEndorsements();
+    }
+}
